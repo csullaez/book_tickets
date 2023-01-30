bc546bfd1a425f76cdce99aa0b61058ca1edf659
Carlos Sullaez <csullaez@gmail.com>
2023-01-30 11:57:07 -0400

    feat: :rocket: Adicion de componentes y primera fase de card para vuelos

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,3 +1,4 @@
+import 'package:book_tickets/screens/ticket_view.dart';
 import 'package:book_tickets/utils/app_styles.dart';
 import 'package:fluentui_icons/fluentui_icons.dart';
 import 'package:flutter/material.dart';
@@ -45,9 +46,10 @@ class Home extends StatelessWidget {
                 Container(
                   decoration: BoxDecoration(
                     borderRadius: BorderRadius.circular(10),
-                    color: const Color(0xFF4F6FD),
+                    color: const Color(0xFFF4F6FD),
                   ),
-                  padding: const EdgeInsets.symmetric(horizontal: 12),
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 12, vertical: 12),
                   child: Row(
                     children: [
                       const Icon(FluentSystemIcons.ic_fluent_search_regular,
@@ -56,9 +58,26 @@ class Home extends StatelessWidget {
                     ],
                   ),
                 ),
+                const Gap(40),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text('Upcoming Flights', style: Styles.headLineStyle2),
+                    InkWell(
+                      onTap: () {
+                        print('View all print');
+                      },
+                      child: Text('View all',
+                          style: Styles.textStyle
+                              .copyWith(color: Styles.primaryColor)),
+                    )
+                  ],
+                )
               ],
             ),
           ),
+          const Gap(15),
+          TicketView(),
         ],
       ),
     );
